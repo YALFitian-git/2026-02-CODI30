2829b7e486601c8f881b874941ec3500dc10e94c
Y.Al Fitian <158461535+YALFitian-git@users.noreply.github.com>
2026-02-10 10:25:46 -0600

    Update numberguess.sh

--- numberguess.sh
+++ numberguess.sh
@@ -3,7 +3,7 @@ clear
 echo Input how big the guessing range should be:
 read maxRangeVal
 clear
-while [[ -z "$maxRangeVal" ]]
+while [[ -z $maxRangeVal ]]
 do
 clear
 echo invalid input, try again:
@@ -12,7 +12,7 @@ done
 randint=$((RANDOM % $maxRangeVal + 1)) #Generate a random number from 1 to the maximum range value set by the user.
 echo Guess a number from 1 to $maxRangeVal:
 read number
-while [ $number != $randint ] || [[ -z "$number" ]]
+while [ $number != $randint ] || [[ -z $number ]]
 do
 if [[ $number < $randint && -n $number ]] #Fixes a bug where giving empty input is seen as lower than the random number.
 then
